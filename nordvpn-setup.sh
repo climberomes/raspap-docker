988d6aa118858a68c485159bc7cffd0df6c19785
climberomes <oalhayek12@gmail.com>
2025-12-10 16:18:20 -0800

    NordVPN needs to start

--- nordvpn-setup.sh
+++ nordvpn-setup.sh
@@ -2,9 +2,15 @@
 
 set -e
 
+# Start NordVPN daemon manually
+/etc/init.d/nordvpn start || true
+sleep 2  # give it a moment
+
+# Login
 echo "[NORDVPN] Logging in..."
 nordvpn login --token "$NORDVPN_TOKEN"
 
+# Whitelist
 echo "[NORDVPN] Adding whitelists..."
 nordvpn whitelist add port 22 || true
 nordvpn whitelist add port 53 || true
